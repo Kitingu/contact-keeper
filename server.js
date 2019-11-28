4ff73832407b9995cce49d63bdb7205d7e1e4f54
benlegendj <benlegendj@gmail.com>
2019-11-28 23:49:54 +0300

    connect mongodb to app

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const authRoutes = require('./routes/auth');
 const contactRoutes = require('./routes/contacts');
 const userRoutes = require('./routes/users');
 const app = express();
+
+// connect Database
+connectDB();
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,9 +15,9 @@ app.get('/', (req, res) => {
 });
 
 // define routes
-app.use('api/users',userRoutes)
-app.use('api/auth',authRoutes)
-app.use('api/contacts',contactRoutes)
+app.use('api/users', userRoutes);
+app.use('api/auth', authRoutes);
+app.use('api/contacts', contactRoutes);
 
 app.listen(PORT, () => {
 	console.log(`app running on port ${PORT} `);

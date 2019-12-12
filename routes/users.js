ca7ae0b461b095530bfd1ae7f9098093d7eb9143
benlegendj <benlegendj@gmail.com>
2019-12-13 02:12:10 +0300

    hash password and register route

--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
-
+const User = require('../models/User');
 router.post(
 	'/',
 	[
 		check('name', 'Name is required')
 			.not()
 			.isEmpty(),
-		check('email', 'Please provide a valid email')
-			.not()
-			.isEmail(),
+		check('email', 'Please provide a valid email').isEmail(),
 		check(
 			'password',
 			'Please provide password with 6 or more characters'
 		).isLength({ min: 6 }),
 	],
-	(req, res) => {
+	async (req, res) => {
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
 			return res.status(400).json({ errors: errors.array() });
 		}
-		res.json({message:'we are past errors'});
+		const { name, email, password } = req.body;
+		try {
+			let user = await User.findOne({ email });
+			if (user) {
+				return res.status(400).json({ message: 'usee already exists' });
+			}
+			user = new User({ name, email, password });
+			const salt = await bcrypt.genSalt(10);
+			user.password = await bcrypt.hash(password, salt);
+			await user.save();
+			res.send('user saved');
+		} catch (error) {
+			console.log(error.message);
+			res.status(500).send('something went wrong');
+		}
 	}
 );
 

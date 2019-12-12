1ef709cd618481ac8056bc055db8759706b83b94
benlegendj <benlegendj@gmail.com>
2019-12-12 21:28:49 +0300

    user model and validation

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-/**
- * @route POST api/users
- * @desc   Register a user
- * @access Public
- */
-router.post('/', (req, res) => {
-	res.send('register a user');
-});
+router.post(
+	'/',
+	[
+		check('name', 'Name is required')
+			.not()
+			.isEmpty(),
+		check('email', 'Please provide a valid email')
+			.not()
+			.isEmail(),
+		check(
+			'password',
+			'Please provide password with 6 or more characters'
+		).isLength({ min: 6 }),
+	],
+	(req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+		res.json({message:'we are past errors'});
+	}
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,24 +1,27 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const morgan = require('morgan');
 const authRoutes = require('./routes/auth');
 const contactRoutes = require('./routes/contacts');
 const userRoutes = require('./routes/users');
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan('dev'));
+
 // connect Database
 connectDB();
 
-const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-	res.send('Hello world');
-});
-
 // define routes
-app.use('api/users', userRoutes);
-app.use('api/auth', authRoutes);
-app.use('api/contacts', contactRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/contacts', contactRoutes);
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
 	console.log(`app running on port ${PORT} `);
 });
+
+module.exports.app = app;

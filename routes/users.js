770c21a83222c2f2b33274a580d785987eb573ce
benlegendj <benlegendj@gmail.com>
2019-12-13 02:36:17 +0300

    send jwt token for auth

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { check, validationResult } = require('express-validator');
+const config = require('config');
 const User = require('../models/User');
 router.post(
 	'/',
@@ -30,7 +32,22 @@ router.post(
 			const salt = await bcrypt.genSalt(10);
 			user.password = await bcrypt.hash(password, salt);
 			await user.save();
-			res.send('user saved');
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			};
+			jwt.sign(
+				payload,
+				config.get('JWT_SECRET'),
+				{
+					expiresIn: 360000,
+				},
+				(err, token) => {
+					if (err) throw error;
+					res.json({ token });
+				}
+			);
 		} catch (error) {
 			console.log(error.message);
 			res.status(500).send('something went wrong');

983a30361c7f1434f7b3a803c8710e7753c90bf3
benlegendj <benlegendj@gmail.com>
2019-12-16 13:54:13 +0300

    setup react-app and concurently

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/contacts', contactRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
 	console.log(`app running on port ${PORT} `);

ca49600eb507c1b3d366c21d0604fc9ed974348a
kitingu <benlegendj@gmail.com>
2020-03-28 23:10:00 +0300

    Register and login forms

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Contacts from "../contacts/Contacts";
 import ContactForm from "../contacts/ContactForm";
-import ContactFilter from "../contacts/contactFilter";
+import ContactFilter from "../contacts/ContactFilter";
 
 const Home = () => {
   return (
